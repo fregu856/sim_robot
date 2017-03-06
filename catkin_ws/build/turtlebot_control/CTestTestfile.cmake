3004b5d4c675080f3795b908164b71683ba056a8
fregu856 <fregu856@student.liu.se>
2017-03-05 20:57:16 -0800

    Setting up a catkin workspace, cloning the ASL turtlebot simulator etc.

---
+++ catkin_ws/build/turtlebot_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fregu856/sim_robot/catkin_ws/src/turtlebot_control
+# Build directory: /home/fregu856/sim_robot/catkin_ws/build/turtlebot_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
